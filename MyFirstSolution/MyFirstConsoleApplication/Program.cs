b012a9cbfe41433521c6c6811761e11e454cea79
Svetlana Yurkova <svyurkova@gmail.com>
2020-11-02 21:01:20 +0300

    454454554

--- MyFirstSolution/MyFirstConsoleApplication/Program.cs
+++ MyFirstSolution/MyFirstConsoleApplication/Program.cs
@@ -7,6 +7,10 @@ namespace MyFirstConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }

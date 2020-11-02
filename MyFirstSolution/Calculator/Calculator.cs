beaa5089edc1871e87262ab35daa4e1ffe386f14
Svetlana Yurkova <svyurkova@gmail.com>
2020-11-02 20:20:39 +0300

    Migrated Calculator project

---
+++ MyFirstSolution/Calculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    static class Calculator
+    {
+        const double Pi = 3.14;
+
+        public static void Sum() 
+        {
+            int numberA = 100; //Camel Case 
+            int numberB = 200;
+
+            int numberC = numberA;
+
+            bool logic = false;
+
+            string text = "Hello, guys!";
+            string textNull = null;
+
+            double lengthC = Pi * numberA;
+
+            numberB = 300;
+
+            Console.WriteLine(100 + 200);
+            Console.WriteLine("New Line");
+            Console.WriteLine(numberA + numberB);
+        } 
+    }
+}
---
+++ MyFirstSolution/Calculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Calculator.Sum();
+
+            //class Math
+            double pi = Math.PI;
+
+            byte byteType = 25;
+            int age = byteType;
+            byte resultValue = (byte)age;
+
+            string txtNumber = "24h";
+            int num = Convert.ToInt32(txtNumber);
+
+            long @long = num;
+
+            int devision = 9 / 7;
+        }
+    }
+}
